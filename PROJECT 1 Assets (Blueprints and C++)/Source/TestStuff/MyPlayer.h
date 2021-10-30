c45bfd4f3c3873a9631cec8f3e5a55e109569ad1
Anna J <53334211+pinacoladakitten@users.noreply.github.com>
2021-10-30 10:39:57 -0400

    Add files via upload

---
+++ PROJECT 1 Assets (Blueprints and C++)/Source/TestStuff/MyPlayer.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/BoxComponent.h"
+#include "MyPlayer.generated.h"
+
+UCLASS()
+class TESTSTUFF_API AMyPlayer : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+		void MoveVertical(float amount);
+	UFUNCTION()
+		void MoveHorizontal(float amount);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float Speed = -4000.f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		UBoxComponent* BoxCollision;
+
+};
