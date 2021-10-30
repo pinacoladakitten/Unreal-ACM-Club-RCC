ca192d3d91872c1bcc9929d01ca544f295ebcc09
Anna J <53334211+pinacoladakitten@users.noreply.github.com>
2021-10-30 10:41:41 -0400

    Add these to your Project's Folder
    Make sure to rename, YOUR_PROJECT_NAME, to your project name lol

---
+++ PROJECT 1 Assets (Blueprints and C++)/Source/YOUR_PROJECT_NAME/MyObstacle.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyObstacle.generated.h"
+
+UCLASS()
+class TESTSTUFF_API AMyObstacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyObstacle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float AngularFrequency = 4.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float Amplitude = 1000.f;
+
+};
